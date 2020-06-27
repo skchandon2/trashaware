7b3dbc2a06035a064fb5d5f933194f4646964519
Shah Chandon <skchandon@yahoo.com>
2020-06-27 00:52:34 -0400

    initial commit with meta and jquery+boostrap

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Trash Aware</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Waste sorting game" />
+    <meta name="keywords" content="waste,experience,trash,sorting,game" />
+    <meta property="og:site_name" content="Trash Aware" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Home | Trash Aware | Game" />
+    <meta property="og:url" content="https:///" />
+    <meta property="og:description" content="Waste sorting gamet" />
+    <meta property="og:updated_time" content="2020-06-27T12:58:50-04:00" />
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
+</head>
+<body>
+    <div class="row">    
+        <h1>Trash Aware</h1>
+        <h4>A trash sorting game!</h4>
+    </div>
+</body>
+</html>
